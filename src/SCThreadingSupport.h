c241a4384e49db7f37f5fc1bb162b9eb23b34631
dave <dave@nutricherry.com>
2023-08-10 08:16:18 +0100

    #11 fix compiler warning.

--- src/SCThreadingSupport.h
+++ src/SCThreadingSupport.h
@@ -54,7 +54,7 @@ inline bool casAtomic(position_ptr_t ptr, position_t expected, position_t newVal
     uint32_t exp = expected;
     return core_util_atomic_cas_u32(ptr, &exp, newVal);
 }
-inline position_t readAtomic(position_ptr_t ptr) { return *(ptr); }
+inline uint32_t readAtomic(position_ptr_t ptr) { return *(ptr); }
 #elif (defined(SC_USE_ARM_ASM_CAS) || defined(ARDUINO_ARCH_STM32)) && !defined(SC_NO_ARM_ASM_CAS)
 #define atomicInitialisationSupport()
 #include <Arduino.h>
